9b230c024324da46544582f63b0a55b43c8b26dd
sandesh079 <sandesh.dahal022@gmail.com>
2024-01-21 12:51:48 +0545

    added hash password in db

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,4 +1,7 @@
 const User = require('../models/user')
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
 const registerNewUser = async(req, res)=>{
     try{
         const existingUser = await User.findOne({phoneNumber: req.body.phoneNumber, email: req.body.email})
@@ -7,6 +10,8 @@ const registerNewUser = async(req, res)=>{
                 msg: "User Already Exist"
             })
         }
+        const hashPass = await bcrypt.hash(req.body.password, saltRounds)
+        req.body.password = hashPass
         await User.create(req.body)
         res.json({
             msg: "Register Successfully"    
@@ -16,5 +21,31 @@ const registerNewUser = async(req, res)=>{
     }
 }
 
-module.exports = {registerNewUser}
+
+
+const loginUser = async(req, res)=>{
+    try{
+        const userDetails = await User.findOne({ email: req.body.email})
+        if(userDetails){
+            const match = await bcrypt.compare(req.body.password, userDetails.password)
+            if(match){
+                res.json({
+                    msg: 'Login successful'
+                })
+            }else{
+                res.status(403).json({
+                    msg: 'Incorrect password'
+                })
+            }
+        }else{
+            res.status(403).json({
+                msg: 'Invalid email address'
+            })
+        }
+    }catch(err){
+        console.log(err)
+    }
+}
+
+module.exports = {registerNewUser, loginUser}
 
